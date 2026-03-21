89b599cfa02621d540c52be34595dc27bd00e975
harsh170699 <harsh.shah11@einfochips.com>
2026-03-21 15:19:40 +0530

    10.1 Simplified Express server setup by removing unused imports and HTTP module wrapper

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const http = require('http');
-const fs = require('fs');
-const url = require('url');
 const express = require('express');
 
 const app = express();
@@ -15,9 +13,9 @@ app.get('/about', (req, res) => {
     return res.send('Hello from About page' + ' hey ' + req.query.name + ' you are ' + req.query.age);
 });
 
-// now below ugly looking function is no more required, hence removed it
+app.listen(8000, () => console.log('Server Started!'));
 
-// this creats a web server for us
-const myServer = http.createServer(app);
+// Express is just a framework, internally it uses HTTP module only
+// const myServer = http.createServer(app);
 
-myServer.listen(8000, () => console.log('Server Started!'));
+// myServer.listen(8000, () => console.log('Server Started!'));
